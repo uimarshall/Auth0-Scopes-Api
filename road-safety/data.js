49f414a11d329ec9a5cfdf7860b68e1ac98d6424
uimarshall <uimarshall@gmail.com>
2019-12-19 20:40:59 +0100

    set up the API

---
+++ road-safety/data.js
@@ -0,0 +1,138 @@
+// Get a list of vehicles
+exports.vehicles = [
+	{
+		model: "Toyota Camry",
+		regdNumber: "XE345TY",
+		dateReviewed: "10-12-2016",
+		score: 8,
+		reviewer: "Ansalem Scott",
+		location: "Maryland"
+	},
+	{
+		model: "Toyota Hiace",
+		regdNumber: "RF345JK",
+		dateReviewed: "11-11-2018",
+		score: 9,
+		reviewer: "Brad Smith",
+		location: "Ojota"
+	},
+	{
+		model: "Murano",
+		regdNumber: "KT345IB",
+		dateReviewed: "06-09-2019",
+		score: 6,
+		reviewer: "Inyang Henshaw",
+		location: "Victoria Island"
+	},
+	{
+		model: "Toyota Camry",
+		regdNumber: "MN604TY",
+		dateReviewed: "10-12-2016",
+		score: 8,
+		reviewer: "Joseph Joseph",
+		location: "Yaba"
+	},
+	{
+		model: "Toyota Camry",
+		regdNumber: "BG675SD",
+		dateReviewed: "10-12-2016",
+		score: 7,
+		reviewer: "Young Ashley",
+		location: "Pamlgrove"
+	},
+	{
+		model: "Toyota Camry",
+		regdNumber: "PO981XC",
+		dateReviewed: "10-12-2016",
+		score: 8,
+		reviewer: "Fred Caro",
+		location: "Ikorodu"
+	},
+	{
+		model: "Toyota Camry",
+		regdNumber: "ZE621CB",
+		dateReviewed: "10-12-2016",
+		score: 8,
+		reviewer: "Ann Singh",
+		location: "Ikeja"
+	}
+];
+
+// Get a list of all of our reviewers
+exports.reviewers = [
+	{
+		name: "Ansalem Scott",
+		publication: "The Daily Reviewer",
+		avatar:
+			"https://s3.amazonaws.com/uifaces/faces/twitter/angelcolberg/128.jpg"
+	},
+	{
+		name: "Brad Smith",
+		publication: "International Movie Critic",
+		avatar: "https://s3.amazonaws.com/uifaces/faces/twitter/bungiwan/128.jpg"
+	},
+	{
+		name: "Inyang Henshaw",
+		publication: "MoviesNow",
+		avatar: "https://s3.amazonaws.com/uifaces/faces/twitter/grrr_nl/128.jpg"
+	},
+	{
+		name: "Joseph Joseph",
+		publication: "MyNextReview",
+		avatar: "https://s3.amazonaws.com/uifaces/faces/twitter/d00maz/128.jpg"
+	},
+	{
+		name: "Young Ashley",
+		publication: "Movies n' Games",
+		avatar: "https://s3.amazonaws.com/uifaces/faces/twitter/laurengray/128.jpg"
+	},
+	{
+		name: "Fred Caro",
+		publication: "TheOne",
+		avatar: "https://s3.amazonaws.com/uifaces/faces/twitter/karsh/128.jpg"
+	},
+	{
+		name: "Ann Singh",
+		publication: "ComicBookHero.com",
+		avatar: "https://s3.amazonaws.com/uifaces/faces/twitter/9lessons/128.jpg"
+	}
+];
+
+// Get a list of publications
+exports.publications = [
+	{ name: "The Daily Reviewer", avatar: "glyphicon-eye-open" },
+	{ name: "International Movie Critic", avatar: "glyphicon-fire" },
+	{ name: "MoviesNow", avatar: "glyphicon-time" },
+	{ name: "MyNextReview", avatar: "glyphicon-record" },
+	{ name: "Movies n' Games", avatar: "glyphicon-heart-empty" },
+	{ name: "TheOne", avatar: "glyphicon-globe" },
+	{ name: "ComicBookHero.com", avatar: "glyphicon-flash" }
+];
+
+// Get a list of pending vehicle reviews
+exports.pending = [
+	{
+		model: "Danfo",
+		regdNumber: "FE324UH",
+		dateReviewed: "10-12-2019",
+		score: 7,
+		reviewer: "Lee Ma",
+		location: "Ogudu"
+	},
+	{
+		model: "Kia",
+		regdNumber: "BG675SD",
+		dateReviewed: "02-08-2019",
+		score: 7,
+		reviewer: "Bin Laden",
+		location: "Ojodu"
+	},
+	{
+		model: "Mercedes Benz",
+		regdNumber: "LJ983VD",
+		dateReviewed: "12-06-2018",
+		score: 7,
+		reviewer: "Ette Boy",
+		location: "Ring road"
+	}
+];
---
+++ road-safety/server/server.js
@@ -0,0 +1,142 @@
+// Get our dependencies
+// 'express - jwt' library will give us functions to work with JSON Web Tokens
+// 'auth0 - api - jwt - rsa - validation' will provide a helper function for getting our secret key.
+const express = require("express");
+const app = express();
+const jwt = require("express-jwt");
+const rsaValidation = require("auth0-api-jwt-rsa-validation");
+const { vehicles, reviewers, publications, pending } = require("../data");
+
+// We’ll create a middleware function to validate the access token when our API is called
+// Note that the audience field is the identifier you gave to your API.
+var jwtCheck = jwt({
+	secret: rsaValidation(),
+	algorithms: ["RS256"],
+	issuer: "https://dev-0ikx9zkh.auth0.com/",
+	audience: "https://roadsafety.com"
+});
+
+// Enable the use of the jwtCheck middleware in all of our routes
+app.use(jwtCheck);
+
+// If we do not get the correct credentials, we’ll return an appropriate message
+app.use((err, req, res, next) => {
+	if (err.name === "UnauthorizedError") {
+		res.status(401).json({ message: "Missing or invalid token" });
+	}
+});
+
+// var jwtCheck = jwt({
+// 	secret: jwks.expressJwtSecret({
+// 		cache: true,
+// 		rateLimit: true,
+// 		jwksRequestsPerMinute: 5,
+// 		jwksUri: "https://dev-0ikx9zkh.auth0.com/.well-known/jwks.json"
+// 	}),
+// 	audience: "https://roadsafety.com",
+// 	issuer: "https://dev-0ikx9zkh.auth0.com/",
+// 	algorithms: ["RS256"]
+// });
+
+app.use(jwtCheck);
+
+var guard = (req, res, next) => {
+	// we’ll use a case switch statement on the route requested
+	switch (req.path) {
+		// if the request is for vehicle reviews we’ll check to see if the token has general scope
+		case "/vehicles": {
+			var permissions = ["general"];
+			for (var i = 0; i < permissions.length; i++) {
+				if (req.user.scope.includes(permissions[i])) {
+					next();
+				} else {
+					res.send(403, { message: "Forbidden" });
+				}
+			}
+			break;
+		}
+		// Same for the reviewers
+		case "/reviewers": {
+			var permissions = ["general"];
+			for (var i = 0; i < permissions.length; i++) {
+				if (req.user.scope.includes(permissions[i])) {
+					next();
+				} else {
+					res.send(403, { message: "Forbidden" });
+				}
+			}
+			break;
+		}
+		// Same for publications
+		case "/publications": {
+			var permissions = ["general"];
+			for (var i = 0; i < permissions.length; i++) {
+				if (req.user.scope.includes(permissions[i])) {
+					next();
+				} else {
+					res.send(403, { message: "Forbidden" });
+				}
+			}
+			break;
+		}
+		// For the pending route, we’ll check to make sure the token has the scope of admin before returning the results.
+		case "/pending": {
+			var permissions = ["admin"];
+			console.log(req.user.scope);
+			for (var i = 0; i < permissions.length; i++) {
+				if (req.user.scope.includes(permissions[i])) {
+					next();
+				} else {
+					res.send(403, { message: "Forbidden" });
+				}
+			}
+			break;
+		}
+	}
+};
+
+// existing app.use middleware
+
+app.use(guard);
+
+// =============================ROUTES============================================
+
+// Implement the vehicles API endpoint
+app.get("/vehicles", function(req, res) {
+	// Get a list of vehicles and their review scores
+	vehicles;
+	// Send the response as a JSON array
+	res.json(vehicles);
+});
+
+// Implement the reviewers API endpoint
+app.get("/reviewers", function(req, res) {
+	// Get a list of all of our reviewers
+	reviewers;
+	// Send the list of reviewers as a JSON array
+	res.json(reviewers);
+});
+
+// Implement the publications API endpoint
+app.get("/publications", function(req, res) {
+	// Get a list of publications
+	publications;
+
+	// Send the list of publications as a JSON array
+	res.json(publications);
+});
+
+// Implement the pending reviews API endpoint
+app.get("/pending", function(req, res) {
+	// Get a list of pending vehicle reviews
+	pending;
+
+	// Send the list of pending vehicle reviews as a JSON array
+	res.json(pending);
+});
+
+// Launch our API Server and have it listen on port 5000.
+const PORT = process.env.port || 5000;
+app.listen(PORT, () => {
+	console.log(`Server started on port ${PORT}`);
+});
